bde1b542a862ab7ce54b294542289b39c9c92e0c
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-02 15:52:32 -0800

    commit tree c646fec9f92738626bd6a3a03f2a5fbcf4704dff

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching (drivers, string) {
+  return drivers.filter(function(el){
+    if (el.toLowerCase() === string.toLowerCase()) {
+      return el
+    } 
+  })
+}
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter(function(el){
+    if  (el.startsWith(string)){
+      return el
+    }
+  })
+}
+
+function matchName (driver, string) {
+  return driver.filter(function(el){
+    if (el.name === string){
+      return el
+    }
+
+  })
+}
